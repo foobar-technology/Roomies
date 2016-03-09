95ec32837018e214a5e12248bca089f07549150b
orlando26 <orlandolozaarr@gmail.com>
2016-03-09 01:25:54 -0600

    Aplicacion JSF

---
+++ JSF/Roomies/src/org/roomies/beans/Main.java
@@ -0,0 +1,19 @@
+package org.roomies.beans;
+
+public class Main extends Form{
+
+	/**
+	 * Main Bean
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public String gotoSecond() {
+		System.out.println("go to second");
+        return "pm:second";
+    }
+	
+	public void print(){
+		System.out.println("imprimir valor");
+	}
+	
+}
