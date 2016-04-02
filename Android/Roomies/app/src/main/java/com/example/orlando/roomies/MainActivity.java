cdbb7d1e056944b7d9506f3c7b8456470952b6dc
orlando <orlandolozaarr@gmail.com>
2016-04-02 12:02:35 -0600

    removed email button

--- Android/Roomies/app/src/main/java/com/example/orlando/roomies/MainActivity.java
+++ Android/Roomies/app/src/main/java/com/example/orlando/roomies/MainActivity.java
@@ -24,14 +24,6 @@ public class MainActivity extends AppCompatActivity {
 
         webSettings.setJavaScriptEnabled(true);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
     @Override
